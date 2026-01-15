3c1034d748e4110270107e3469e2b27e14d62069
Pedro Duarte Neira <pneirag25@fpcoruna.afundacion.org>
2026-01-15 10:28:38 +0100

    conexion registro a login y fallo ortográfico

--- app/src/main/java/com/example/ferrol_app/Foro.java
+++ app/src/main/java/com/example/ferrol_app/Foro.java
@@ -3,22 +3,23 @@ package com.example.ferrol_app;
 public class Foro {
     private int id;
     private String titulo;
-    private String descripcion;
+    private String contenido;
 
-    public Foro(int id, String titulo, String descripcion) {
+    public Foro(int id, String titulo, String contenido) {
         this.id = id;
         this.titulo = titulo;
-        this.descripcion = descripcion;
+        this.contenido = contenido;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getTitulo() {
         return titulo;
     }
-    public int getId() {
-        return id;
-    }
 
-    public String getDescripcion() {
-        return descripcion;
+    public String getContenido() {
+        return contenido;
     }
 }
